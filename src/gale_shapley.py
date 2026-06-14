63380fcdde59df83a0a0bdd4a571c60293b2fdca
lucasarruda12 <lucas123manoel4@gmail.com>
2026-06-14 16:11:43 -0300

    gs

--- src/gale_shapley.py
+++ src/gale_shapley.py
@@ -6,31 +6,29 @@ def pref_to_rank(pref):
         for a, a_pref in pref.items()
     }
 
-def gale_shapley(A, B, A_pref, B_pref):
-    """
-    pref é um dicionário com cada elemento de A e sua respectiva
-    lista de preferências.
-    """
-    B_rank = pref_to_rank(B_pref) # Em alguns momentos é mais fácil usar o ranking
-    a_list = {a: deque(pref_list) for a, pref_list in A_pref.items()}
+def gale_shapley(H, M, H_pref, M_pref):
+    rejeitados = set(H)
+    proxima_opcao = {h : 0 for h in H}
+    em_consideracao = {m : None for m in M}
+    M_rank = pref_to_rank(M_pref)
 
-    pair = {}
-    remaining_A = set(A)
-    while len(remaining_A) > 0: # São feitas propostas até todo mundo estar casado
-        a = remaining_A.popleft()
-        b = a_list[a].popleft() # a primeiro propõem para a sua primeira opção b,
-        # que não será mais considerada (para a)
+    while rejeitados:
+        h = rejeitados.pop()
+        m = H_pref[h][proxima_opcao[h]]
+        proxima_opcao[h] = proxima_opcao[h] + 1
 
-        if b not in pair:
-            pair[b] = a # se b está livre, casamos os dois por enquanto
-        
-        else: # se b já está casado
-            a_current = pair[b]
-            b_prefer_current = B_rank[b][a_current] < B_rank[b][a]
-            if b_prefer_current:
-                remaining_A.add(a) # a pior que o marido atual, não trocamos
-            else: # a melhor que o marido atual
-                pair[b] = a # trocamos
-                remaining_A.add(a_current) # o antigo marido de b volta para a lista de solteiros.
-    
-    return [(a, b) for b, a in pair.items()]
+        h2 = em_consideracao[m]
+
+        if (h2 == None):
+            em_consideracao[m] = h
+
+        elif M_rank[m][em_consideracao[m]] < M_rank[m][h]:
+            rejeitados.add(em_consideracao[m])
+            em_consideracao[m] = h
+
+        else:
+            rejeitados.add(h)
+
+    return [(h, m) for m, h in em_consideracao.items()]
+
+
